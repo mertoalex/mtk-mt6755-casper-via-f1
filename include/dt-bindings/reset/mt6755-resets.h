24c5b5269fc4674f1f43cf4155e34a3ce05f7376
mertoalex <baytemurmuslum+github@gmail.com>
2023-10-14 17:50:16 +0300

    FORK https://gitlab.com/mtk-mainline/mt6755/linux

---
+++ include/dt-bindings/reset/mt6755-resets.h
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2023 Arseniy Velikanov <adomerlee@gmail.com>
+ */
+
+#ifndef _DT_BINDINGS_RESET_CONTROLLER_MT6755
+#define _DT_BINDINGS_RESET_CONTROLLER_MT6755
+
+/* FIXME: Add other needed MT6755_TOPRGU_* #defines! */
+
+#define MT6755_TOPRGU_SW_RST_NUM               20
+
+#endif  /* _DT_BINDINGS_RESET_CONTROLLER_MT6755 */
